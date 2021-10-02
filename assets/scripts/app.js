b86348657c524cbb9fb61ea9375da50f329a7bad
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-10-01 22:15:19 -0700

    add submit handler and basic user input validation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,9 @@
 const backdrop = document.getElementById("backdrop");
 const addMovieModal = document.getElementById("add-modal");
-const movieAddButton = document.body.querySelector("header button");
-const movieCancelButton = document.body.querySelector(".btn--passive");
+const movieAddButton = document.querySelector("header button");
+const movieCancelButton = document.querySelector(".btn--passive");
+const movieSubmitButton = document.querySelector(".btn--success");
+const allInputs = addMovieModal.querySelectorAll("input");
 
 const toggleBackdropClickHandler = () => {
   addMovieClickhandler();
@@ -16,6 +18,24 @@ const cancelMovieClickHandler = () => {
   addMovieClickhandler();
 };
 
+const submitMovieClickHandler = () => {
+  const titleInput = allInputs[0].value;
+  const urlInput = allInputs[1].value;
+  const ratingInput = allInputs[2].value;
+
+  if (
+    titleInput.trim() === "" ||
+    urlInput.trim() === "" ||
+    ratingInput.trim() === "" ||
+    +ratingInput < 1 ||
+    +ratingInput > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5).");
+    return;
+  }
+};
+
 backdrop.addEventListener("click", toggleBackdropClickHandler);
 movieAddButton.addEventListener("click", addMovieClickhandler);
-movieCancelButton.addEventListener("click", addMovieClickhandler);
+movieCancelButton.addEventListener("click", cancelMovieClickHandler);
+movieSubmitButton.addEventListener("click", submitMovieClickHandler);
